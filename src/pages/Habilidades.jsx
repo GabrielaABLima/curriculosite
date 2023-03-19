a6d0c77e509be7e32c22dbebf6927e0e10dbe3f1
GabrielaABLima <gabriela.andrade.bueno.lima@gmail.com>
2023-03-19 08:21:03 -0300

    mudança

--- src/pages/Habilidades.jsx
+++ src/pages/Habilidades.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Fade } from 'react-reveal';
 
 function Habilidades() {
   return (
@@ -9,52 +8,38 @@ function Habilidades() {
           
             <div className='fundoCompleto'>
               <p className='titulo'>Java</p>
-              <Fade left>
                 <div className='fundoPreenchido' style={{width: '33vw'}}></div>
-              </Fade>
             </div>
           
           
           <div className='fundoCompleto'>
             <p className='titulo'>HTML/CSS</p>
-            <Fade left>
                 <div className='fundoPreenchido' style={{width: '35vw'}}></div>
-            </Fade>
           </div>
 
           <div className='fundoCompleto'>
             <p className='titulo'>JavaScript</p>
-            <Fade left>
                 <div className='fundoPreenchido' style={{width: '30vw'}}></div>
-            </Fade>
           </div>
 
           <div className='fundoCompleto'>
             <p className='titulo'>React</p>
-            <Fade left>
                 <div className='fundoPreenchido' style={{width: '25vw'}}></div>
-              </Fade>
           </div>
 
           <div className='fundoCompleto'>
             <p className='titulo'>SQL</p>
-            <Fade left>
                 <div className='fundoPreenchido' style={{width: '34vw'}}></div>
-              </Fade>
           </div>
 
           <div className='fundoCompleto'>
             <p className='titulo'>Python</p>
-            <Fade left>
                 <div className='fundoPreenchido' style={{width: '30vw'}}></div>
-              </Fade>
           </div>
 
           <div className='fundoCompleto'>
             <p className='titulo'>Android Studio</p>
-            <Fade left>
                 <div className='fundoPreenchido' style={{width: '34vw'}}></div>
-              </Fade>
           </div>
         </div>
         
